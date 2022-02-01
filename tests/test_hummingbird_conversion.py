b9cc1e8f21ce0a4723d1fa48c363e7d3fd85b557
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2022-02-01 19:30:35 +0100

    Add tests

--- tests/test_hummingbird_conversion.py
+++ tests/test_hummingbird_conversion.py
@@ -2,10 +2,13 @@
 Example for conversion of a hipe4ml ModelHandler to ONNX format
 """
 
+import os
 import sys
 import pandas as pd
 import numpy as np
 import xgboost as xgb
+from hummingbird.ml.containers.sklearn.onnx_containers import ONNXSklearnContainerClassification
+from hummingbird.ml.containers.sklearn.pytorch_containers import PyTorchSklearnContainerClassification
 from sklearn.model_selection import train_test_split
 from hipe4ml.model_handler import ModelHandler
 
@@ -24,9 +27,45 @@ model_handler = ModelHandler(model_clf, df_sgn.columns)
 model_handler.train_test_model(train_test_data, True, output_margin="raw")
 
 model_converter = H4MLConverter(model_handler)
-model_converter.convert_model_hummingbird("onnx", 1)
-model_converter.dump_model_hummingbird("model_onnx")
-model_converter.convert_model_hummingbird("pytorch", 1)
-model_converter.dump_model_hummingbird("model_pytorch")
-model_converter.convert_model_hummingbird("torch", 1)
-model_converter.dump_model_hummingbird("model_torch")
+
+def test_convert_model_hummingbird_onnx():
+    """
+    Test the hummingbird conversion to onnx
+    """
+    assert isinstance(model_converter.convert_model_hummingbird("onnx", 1), ONNXSklearnContainerClassification)
+
+def test_dump_model_hummingbird_onnx():
+    """
+    Test the dump of the onnx file
+    """
+    model_converter.dump_model_hummingbird("model_onnx")
+    assert os.path.isfile("model_onnx.zip")
+    os.remove("model_onnx.zip")
+
+def test_convert_model_hummingbird_pytorch():
+    """
+    Test the hummingbird conversion to pytorch
+    """
+    assert isinstance(model_converter.convert_model_hummingbird("pytorch", 1), PyTorchSklearnContainerClassification)
+
+def test_dump_model_hummingbird_pytorch():
+    """
+    Test the dump of the pytorch file
+    """
+    model_converter.dump_model_hummingbird("model_pytorch")
+    assert os.path.isfile("model_pytorch.zip")
+    os.remove("model_pytorch.zip")
+
+def test_convert_model_hummingbird_torch():
+    """
+    Test the hummingbird conversion to torch
+    """
+    assert isinstance(model_converter.convert_model_hummingbird("torch", 1), PyTorchSklearnContainerClassification)
+
+def test_dump_model_hummingbird_torch():
+    """
+    Test the dump of the torch file
+    """
+    model_converter.dump_model_hummingbird("model_torch")
+    assert os.path.isfile("model_torch.zip")
+    os.remove("model_torch.zip")
--- tests/test_onnx_conversion.py
+++ tests/test_onnx_conversion.py
@@ -2,11 +2,13 @@
 Example for conversion of a hipe4ml ModelHandler to ONNX format
 """
 
+import os
 import sys
 import pandas as pd
 import numpy as np
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
+from onnx import onnx_ml_pb2
 from hipe4ml.model_handler import ModelHandler
 
 sys.path.append("../hipe4ml_converter") # needed because hipe4ml-converter is not installed
@@ -24,5 +26,17 @@ model_handler = ModelHandler(model_clf, df_sgn.columns)
 model_handler.train_test_model(train_test_data, True, output_margin="raw")
 
 model_converter = H4MLConverter(model_handler)
-model_converter.convert_model_onnx(1)
-model_converter.dump_model_onnx("model.onnx")
+
+def test_convert_model_onnx():
+    """
+    Test the conversion to onnx
+    """
+    assert isinstance(model_converter.convert_model_onnx(1), onnx_ml_pb2.ModelProto)
+
+def test_dump_model_onnx():
+    """
+    Test the dump of the onnx file
+    """
+    model_converter.dump_model_onnx("model.onnx")
+    assert os.path.isfile("model.onnx")
+    os.remove("model.onnx")
